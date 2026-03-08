cff97003ad43ea490e65accddf556c8c520770bf
Kristian Lubina <kristian.lubina@outlook.com>
2026-03-08 01:31:40 +0100

    init

---
+++ study-visualization/program-specific/eth-itet/data/basic-modules-details.js
@@ -0,0 +1,260 @@
+window.StudiengangModuleDetails = {
+  // #region 1 Semester
+  "Lineare Algebra": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=192867&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-math/2015/autumn/401-1151-00L/1707a212-2905-448f-b042-6eca940f6c93.html",
+    pruefungen: [
+      {
+        label: "HS20 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Lineare-Algebra/Lineare-Algebra-HS20-Exam-Analyzed.pdf",
+      },
+      {
+        label: "Zusammenfassung (Lukas Cavigelli)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Lineare-Algebra/Zusammenfassung%20Lineare%20Algebra%20ITET%20Lukas%20Cavigelli.pdf",
+      },
+    ],
+  },
+  "Analysis 1": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193410&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-math/2016/autumn/401-1261-07L/a65b10a4-c135-3bca-a4f5-c37d9f8b0913.html",
+    pruefungen: [
+      {
+        label: "FS23 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Analysis-I/Analysis-I-FS23-Exam-Analyzed.pdf",
+      },
+      {
+        label: "Zusammenfassung (Jeremias Baur)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Analysis-I/Analysis_1_ZSF_JeremiasBaur.pdf",
+      },
+    ],
+  },
+
+  Digitaltechnik: {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=192958&lang=de",
+    pruefungen: [
+      {
+        label: "HS18 Aufgabenblatt",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Digitaltechnik/HS18_Digitaltechnik_Aufgabenblatt.pdf",
+      },
+      {
+        label: "HS18 Aufgabenstellung",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Digitaltechnik/HS18_Digitaltechnik_Aufgabenstellung.pdf",
+      },
+      {
+        label: "Zusammenfassung (Andrej Scheuer)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Digitaltechnik/Digitaltechnik_ZSF_Andrej_Scheuer.pdf",
+      },
+    ],
+  },
+  "Digitaltechnik Praktikum": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193802&lang=de",
+  },
+  "Netzwerke und Schaltungen I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=192802&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+    pruefungen: [
+      {
+        label: "FS20 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Netzwerk-Schaltungen-I/NuS1-FS20-Exam-Analyzed.pdf",
+      },
+    ],
+  },
+  "Technische Mechanik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193886&lang=de",
+    pruefungen: [
+      {
+        label: "FS22 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Technische-Mechanik/TechMech-FS22-Exam-Analyzed.pdf",
+      },
+      {
+        label: "Zusammenfassung (Lina DeWindt)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/1.Sem/Technische-Mechanik/TechMech%20ZSF%20Lina%20DeWindt.pdf",
+      },
+    ],
+  },
+  "Vorkurs Informatik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193009&lang=de",
+    kurslink: "https://lec.inf.ethz.ch/itet/informatik0/2019/",
+  },
+  // #endregion 1 Semester
+
+  // #region 2 Semester
+  "Analysis 2": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=188523&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-math/2016/spring/401-1262-07L/e9902307-e2b7-44f3-8feb-115773280c13.html",
+    pruefungen: [
+      {
+        label: "FS24 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Analysis-II/Analysis-II-FS24-Exam-Analyzed.pdf",
+      },
+      {
+        label: "Zusammenfassung (Ananya)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Analysis-II/Analysis_ZF_Ananya_V5.pdf",
+      },
+    ],
+  },
+  "Netzwerke und Schaltungen II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=187813&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-itet/2023/spring/227-0002-00L",
+    pruefungen: [
+      {
+        label: "FS17 Aufgabenkatalog",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/NuS-2/NuS2_FS17_Aufgabenkatalog.pdf",
+      },
+    ],
+  },
+  "Netzwerke und Schaltungen Praktikum": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=188187&lang=de",
+  },
+  "Physik I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=188037&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-phys/2023/autumn/402-0083-00L.html",
+    pruefungen: [
+      {
+        label: "HS21 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Physik-I/Physik-I-HS21-Exam-Analyzed.pdf",
+      },
+    ],
+  },
+  "Informatik I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=187917&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-infk/2016/autumn/252-0847-00L.html",
+    pruefungen: [
+      {
+        label: "FS18 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Informatik-I/Inf-I-FS18-Exam-Analyzed.pdf",
+      },
+      {
+        label: "Zusammenfassung (Lukas Cavigelli)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Informatik-I/Zusammenfassung%20Informatik%20ITET%20Lukas%20Cavigelli.pdf",
+      },
+    ],
+  },
+  "Mathematische Methoden": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=188879&lang=de",
+    pruefungen: [
+      {
+        label: "FS23 Prüfung (analysiert)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Mathematische-Methoden/Mathematische-Methoden-FS23-Exam%2BAnalyzed.pdf",
+      },
+      {
+        label: "Zusammenfassung Komplexe Analysis (Lukas Cavigelli)",
+        url: "https://raw.githubusercontent.com/KLubina/FavoriteStudiesHub/main/old-exams/ITET-old-exams/2.Sem/Mathematische-Methoden/Zusammenfassung%20Komplexe%20Analysis%20ITET%20Lukas%20Cavigelli.pdf",
+      },
+    ],
+  },
+  // #endregion 2 Semester
+
+  // #region 3 Semester
+  "Analysis 3": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193965&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-math/2015/autumn/401-0363-10L/a36b37a3-3ac0-4e47-8310-b79347db3717.html",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/3.Sem/Analysis-III",
+  },
+  "Physik II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193671&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-phys/2017/spring/402-1782-00L.html",
+  },
+  "Signal- und Systemtheorie I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193407&lang=de",
+    kurslink: "https://www.mins.ee.ethz.ch/teaching/signal/klausuren.html",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/3.Sem/Signal-System-I",
+  },
+  "Diskrete Mathematik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=194088&lang=de",
+    kurslink:
+      "https://cadmo.ethz.ch/education/lectures/HS15/DiskreteMathematik.html\nhttps://crypto.ethz.ch/teaching/DM23/",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/3.Sem/Diskrete-Mathematik",
+  },
+  "Halbleiter-Schaltungstechnik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193624&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-itet/2022/autumn/227-0077-10L",
+  },
+  "Halbleiter-Schaltungstechnik Praktikum": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=192771&lang=de",
+  },
+  // #endregion 3 Semester
+
+  // #region 4 Semester
+  "Signal- und Systemtheorie II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=187298&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-itet/2023/spring/227-0046-10L",
+  },
+  "Informatik II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025W&ansicht=KATALOGDATEN&lerneinheitId=193493&lang=de",
+    kurslink: "https://vs.inf.ethz.ch/edu/I2/",
+  },
+  "Technische Informatik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=187713&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-itet/2023/spring/227-0013-00L",
+  },
+  "Wahrscheinlichkeitstheorie und Statistik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=187991&lang=de",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/4.Sem/Wahrscheinlichkeitstheorie-und-Statistik",
+  },
+  "Elektromagnetische Felder und Wellen": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=188239&lang=de",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/4.Sem/Elektromagnetische-Felder-und-Wellen",
+  },
+  Halbleiterelemente: {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=187791&lang=de",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/4.Sem/Halbleiterelemente",
+  },
+  "Numerische Methoden": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2025S&ansicht=KATALOGDATEN&lerneinheitId=188146&lang=de",
+    vorlesungslink:
+      "https://video.ethz.ch/lectures/d-math/2016/autumn/401-0663-00L/1e9ea34b-4fc6-4dab-8e3b-dd21bfb732df.html",
+    pruefungen:
+      "https://github.com/KLubina/FavoriteStudiesHub/tree/main/old-exams/ITET-old-exams/4.Sem/Numerische-Methoden",
+  },
+  // #endregion 4 Semester
+
+  // #region Wahlbereich
+  "Discrete Event Systems": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=227-0102-00L",
+  },
+
+  "Communication Systems": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=227-0121-00L",
+  },
+
+  "Embedded Systems": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=227-0124-00L",
+  },
+
+  "Communication Networks": {
+    lernziel:
+      "- understand how the Internet works;\n" +
+      "- build and operate Internet-like network infrastructures;\n" +
+      "- identify the right set of metrics to evaluate the performance or the adequacy of a network and " +
+      "- propose ways to improve it (if any).\n\n",
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=227-0120-00L",
+  },
+
+  "Fundamentals of Computer Architecture": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=227-0152-00L",
+  },
+  // #endregion Wahlbereich
+
+  // #region Bachelorarbeit
+  Bachelorarbeit: {
+    link: "https://ee.ethz.ch/de/studium/bachelorstudiengang/bsc-etit/drittes-studienjahr/bachelor-projekt.html",
+  },
+  // #endregion Bachelorarbeit
+};
